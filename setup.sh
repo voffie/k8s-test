ae82923c84a7e87c8d51b27ff2e21ccd6df45fa5
voffie <viktorsoderborg@gmail.com>
2026-02-19 16:11:34 +0100

    Update setup script to use Argo instead of helm for app

--- setup.sh
+++ setup.sh
@@ -4,17 +4,36 @@ APP_VERSION="1.0-SNAPSHOT"
 
 docker build -t k8s-test:$APP_VERSION .
 
+echo "Applying ArgoCD"
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+echo "Waiting for ArgoCD to start"
+kubectl wait --namespace argocd \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/name=argocd-server \
+  --timeout=120s
+
+echo "Applying ArgoCD applications"
+kubectl apply -f k8s/argocd
+
 echo "Applying prometheus"
 kubectl apply -f k8s/prometheus
 
 echo "Applying loki"
 kubectl apply -f k8s/loki
 
-echo "Deploying app using Helm"
-helm upgrade --install k8s-test ./k8s-test --set image.tag=$APP_VERSION
-
 echo "Applying promtail"
 kubectl apply -f k8s/promtail
 
 echo "Applying grafana"
 kubectl apply -f k8s/grafana
+
+ARGOCD_PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret \
+  -o jsonpath="{.data.password}" | base64 -d)
+
+echo "ArgoCD UI available at https://localhost:8080"
+echo "Username: admin"
+echo "Password: $ARGOCD_PASSWORD"
+
+kubectl port-forward svc/argocd-server -n argocd 8080:443
